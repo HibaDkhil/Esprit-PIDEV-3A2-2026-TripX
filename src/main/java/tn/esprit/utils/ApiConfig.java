a4c610eaa65dfb4a3591a27e05434d71b94ac067
SeifMeddebb <Seifeddine.meddeb@esprit.tn>
2026-02-24 10:37:44 +0100

    apis and advanced features

--- src/main/java/tn/esprit/utils/ApiConfig.java
+++ src/main/java/tn/esprit/utils/ApiConfig.java
@@ -8,10 +8,10 @@ public class ApiConfig {
 
     // Stripe Test Mode Secret Key
     // Get yours at: https://dashboard.stripe.com/test/apikeys
-    public static final String STRIPE_SECRET_KEY = "sk_test_51T2vavPVlBCtS0sumOFSUdbV1AWUmoDquoC1d2dqCcZUrJhaqGO37haphQsvTNGdMNem4W9aXiS2EDNUx7XI8z0I006JCJxufs";
+    public static final String STRIPE_SECRET_KEY = "";
 
     // Stripe Test Mode Publishable Key
-    public static final String STRIPE_PUBLISHABLE_KEY = "pk_test_51T2vavPVlBCtS0su1Ezrmk51b72n3wZGybpcEEf4CTwpulvs2QhvqjFhK3mS0wh8IB6xHReIDLFgpaFwvLH8uJnU00Mu3orRvE";
+    public static final String STRIPE_PUBLISHABLE_KEY = "";
 
     // App name shown on Stripe Checkout
     public static final String APP_NAME = "TripX Travel";
@@ -22,7 +22,7 @@ public class ApiConfig {
 
     // OpenWeatherMap API
     // Get yours at: https://openweathermap.org/api
-    public static final String OPENWEATHER_API_KEY = "c58259a074febdf474dd8c4a1b58ad0e";
+    public static final String OPENWEATHER_API_KEY = "";
     public static final String OPENWEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
     // REST Countries API (no key needed)
